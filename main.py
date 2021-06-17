f2099a9b115607d1adea4da9d277580136325e6b
lenaWitterauf <lena.emma77@gmail.com>
2021-06-17 15:08:40 +0100

    Disabled logging for matplotlib fonts

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from src.features import preprocessing, sequences
 from src.training import models
 
 logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("matplotlib.font_manager").disabled = True
+
 
 def _log_all_configs_to_mlflow():
     for config in [
@@ -19,13 +21,11 @@ def _log_all_configs_to_mlflow():
             full_config_name = config.__class__.__name__ + config_name
             mlflow.log_param(full_config_name, str(config_value))
 
+
 if __name__ == "__main__":
     mlflow.set_experiment("Domain Guided Monitoring")
     with mlflow.start_run() as run:
         _log_all_configs_to_mlflow()
         runner = ExperimentRunner(run.info.run_id)
         runner.run()
-
-
-
 
