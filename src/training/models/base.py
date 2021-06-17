bc0fd6fe31379dae5153694a5bfd20f45b77d1eb
lenaWitterauf <lena.emma77@gmail.com>
2021-06-17 15:13:46 +0100

    Added distribute strategy and best weight callback to base model and reformatted model code

--- src/training/models/base.py
+++ src/training/models/base.py
@@ -1,43 +1,66 @@
 from src.features.sequences.transformer import SequenceMetadata
 import tensorflow as tf
 from typing import Any, List, Dict
-from .metrics import MulticlassAccuracy, MulticlassTrueNegativeRate, MulticlassTruePositiveRate
+from .metrics import (
+    MulticlassAccuracy,
+    MulticlassTrueNegativeRate,
+    MulticlassTruePositiveRate,
+)
 from .config import ModelConfig
-from .callbacks import MLFlowCallback
+from .callbacks import MLFlowCallback, BestModelRestoreCallback
 from .initializers import FastTextInitializer
 import logging
 import mlflow
+
 
 class BaseEmbedding:
     config: ModelConfig
     num_features: int
     num_hidden_features: int
 
-    basic_feature_embeddings: tf.Variable # shape: (num_features, embedding_size)
-    basic_hidden_embeddings: tf.Variable # shape: (num_hidden_features, embedding_size)
-    
-    embedding_mask: tf.Variable # shape: (num_features, num_all_features, 1)
+    basic_feature_embeddings: tf.Variable  # shape: (num_features, embedding_size)
+    basic_hidden_embeddings: tf.Variable  # shape: (num_hidden_features, embedding_size)
+
+    embedding_mask: tf.Variable  # shape: (num_features, num_all_features, 1)
 
     def _final_embedding_matrix(self):
         """Overwrite this in case embedding uses attention mechanism etc"""
         return self.basic_feature_embeddings
 
-    def _get_initializer(self, initializer_name: str, initializer_seed: int, description_vocab: Dict[int, str]) -> tf.keras.initializers.Initializer:
-        if initializer_name == 'random_uniform':
+    def _get_initializer(
+        self,
+        initializer_name: str,
+        initializer_seed: int,
+        description_vocab: Dict[int, str],
+    ) -> tf.keras.initializers.Initializer:
+        if initializer_name == "random_uniform":
             return tf.keras.initializers.GlorotUniform(seed=initializer_seed)
-        elif initializer_name == 'random_normal':
+        elif initializer_name == "random_normal":
             return tf.keras.initializers.GlorotNormal(seed=initializer_seed)
-        elif initializer_name == 'fasttext':
+        elif initializer_name == "fasttext":
             initializer = FastTextInitializer(self.config.embedding_dim)
             return initializer.get_initializer(description_vocab)
         else:
-            logging.error('Unknown initializer %s', initializer_name)  
+            logging.error("Unknown initializer %s", initializer_name)
 
-    def _get_feature_initializer(self, description_vocab: Dict[int, str]) -> tf.keras.initializers.Initializer:
-        return self._get_initializer(self.config.feature_embedding_initializer, self.config.feature_embedding_initializer_seed, description_vocab)
+    def _get_feature_initializer(
+        self, description_vocab: Dict[int, str]
+    ) -> tf.keras.initializers.Initializer:
+        return self._get_initializer(
+            self.config.feature_embedding_initializer,
+            self.config.feature_embedding_initializer_seed,
+            description_vocab,
+        )
 
-    def _get_hidden_initializer(self, description_vocab: Dict[int, str]) -> tf.keras.initializers.Initializer:
-        return self._get_initializer(self.config.hidden_embedding_initializer, self.config.hidden_embedding_initializer_seed, description_vocab)
+    def _get_hidden_initializer(
+        self, description_vocab: Dict[int, str]
+    ) -> tf.keras.initializers.Initializer:
+        return self._get_initializer(
+            self.config.hidden_embedding_initializer,
+            self.config.hidden_embedding_initializer_seed,
+            description_vocab,
+        )
+
 
 class BaseModel:
     def __init__(self):
@@ -46,42 +69,80 @@ class BaseModel:
         self.metrics: List[tf.keras.metrics.Metric] = []
         self.config = ModelConfig()
 
-    def _get_embedding_layer(self, metadata: SequenceMetadata, knowledge: Any) -> tf.keras.Model:
+    def _get_embedding_layer(
+        self, metadata: SequenceMetadata, knowledge: Any
+    ) -> tf.keras.Model:
         raise NotImplementedError("This should be implemented by the subclass!!!")
 
-    def build(self, metadata: SequenceMetadata, knowledge: Any):       
-        self.embedding_layer = self._get_embedding_layer(metadata, knowledge)
-        mlflow.log_metric('num_features', self.embedding_layer.num_features)
-        mlflow.log_metric('num_hidden_features', self.embedding_layer.num_hidden_features)
-        
-        self.prediction_model = tf.keras.models.Sequential([
-            tf.keras.layers.Input(shape=(metadata.max_x_length, len(metadata.x_vocab))),
-            self.embedding_layer,
-            self._get_rnn_layer(),
-            tf.keras.layers.Dense(len(metadata.y_vocab), activation='relu'),
-        ])
+    def _select_distribute_strategy(self) -> tf.distribute.Strategy:
+        if self.config.distribute_strategy == "mirrored":
+            return tf.distribute.MirroredStrategy()
+        elif self.config.distribute_strategy.startswith("/gpu"):
+            return tf.distribute.OneDeviceStrategy(
+                device=self.config.distribute_strategy
+            )
+        else:
+            return tf.distribute.get_strategy()
+
+    def build(self, metadata: SequenceMetadata, knowledge: Any):
+        self.strategy = self._select_distribute_strategy()
+        logging.info(
+            "Using strategy with %d workers", self.strategy.num_replicas_in_sync
+        )
+
+        with self.strategy.scope():
+            self.embedding_layer = self._get_embedding_layer(metadata, knowledge)
+            mlflow.log_metric("num_features", self.embedding_layer.num_features)
+            mlflow.log_metric(
+                "num_hidden_features", self.embedding_layer.num_hidden_features
+            )
+
+            self.prediction_model = tf.keras.models.Sequential(
+                [
+                    tf.keras.layers.Input(
+                        shape=(metadata.max_x_length, len(metadata.x_vocab))
+                    ),
+                    self.embedding_layer,
+                    self._get_rnn_layer(),
+                    tf.keras.layers.Dense(len(metadata.y_vocab), activation="relu"),
+                ]
+            )
 
     def _get_rnn_layer(self):
-        if self.config.rnn_type == 'rnn':
+        if self.config.rnn_type == "rnn":
             return tf.keras.layers.SimpleRNN(units=self.config.rnn_dim)
-        elif self.config.rnn_type == 'lstm':
+        elif self.config.rnn_type == "lstm":
             return tf.keras.layers.LSTM(units=self.config.rnn_dim)
-        elif self.config.rnn_type == 'gru':
+        elif self.config.rnn_type == "gru":
             return tf.keras.layers.GRU(units=self.config.rnn_dim)
         else:
-            logging.error('Unknown rnn layer type: %s', self.config.rnn_type)
+            logging.error("Unknown rnn layer type: %s", self.config.rnn_type)
 
-    def train_dataset(self, train_dataset: tf.data.Dataset, test_dataset: tf.data.Dataset, multilabel_classification: bool, n_epochs: int):
-        if multilabel_classification:
-            self._compile_multilabel()
-        else: 
-            self._compile_multiclass()
-        
-        self.history = self.prediction_model.fit(
-            train_dataset, 
-            validation_data=test_dataset,
-            epochs=n_epochs,
-            callbacks=[MLFlowCallback()])
+    def train_dataset(
+        self,
+        train_dataset: tf.data.Dataset,
+        test_dataset: tf.data.Dataset,
+        multilabel_classification: bool,
+        n_epochs: int,
+    ):
+        with self.strategy.scope():
+            if multilabel_classification:
+                self._compile_multilabel()
+            else:
+                self._compile_multiclass()
+
+            self.history = self.prediction_model.fit(
+                train_dataset,
+                validation_data=test_dataset,
+                epochs=n_epochs,
+                callbacks=[
+                    MLFlowCallback(),
+                    BestModelRestoreCallback(
+                        metric=self.config.best_model_metric,
+                        minimize=self.config.best_model_metric_minimize,
+                    ),
+                ],
+            )
 
     def _compile_multilabel(self):
         self.metrics = [
@@ -90,17 +151,24 @@ class BaseModel:
             MulticlassTruePositiveRate(),
         ]
         self.prediction_model.compile(
-            loss=tf.keras.losses.BinaryCrossentropy(), 
-            optimizer=tf.optimizers.Adam(), 
-            metrics=self.metrics)
+            loss=tf.keras.losses.BinaryCrossentropy(),
+            optimizer=tf.optimizers.Adam(),
+            metrics=self.metrics,
+        )
 
     def _compile_multiclass(self):
         self.metrics = [
             tf.keras.metrics.CategoricalAccuracy(),
-            tf.keras.metrics.TopKCategoricalAccuracy(k=5, name='top_5_categorical_accuracy'),
-            tf.keras.metrics.TopKCategoricalAccuracy(k=10, name='top_10_categorical_accuracy'),
+            tf.keras.metrics.TopKCategoricalAccuracy(
+                k=5, name="top_5_categorical_accuracy"
+            ),
+            tf.keras.metrics.TopKCategoricalAccuracy(
+                k=10, name="top_10_categorical_accuracy"
+            ),
         ]
         self.prediction_model.compile(
-            loss=tf.keras.losses.CategoricalCrossentropy(), 
-            optimizer=tf.optimizers.Adam(), 
-            metrics=self.metrics)
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            optimizer=tf.optimizers.Adam(),
+            metrics=self.metrics,
+        )
+
--- src/training/models/causal.py
+++ src/training/models/causal.py
@@ -7,8 +7,8 @@ from src.features.knowledge import CausalityKnowledge
 from .base import BaseModel, BaseEmbedding
 from .config import ModelConfig
 
-class CausalityEmbedding(tf.keras.Model, BaseEmbedding):
 
+class CausalityEmbedding(tf.keras.Model, BaseEmbedding):
     def __init__(self, causality: CausalityKnowledge, config: ModelConfig):
         super(CausalityEmbedding, self).__init__()
         self.config = config
@@ -16,39 +16,50 @@ class CausalityEmbedding(tf.keras.Model, BaseEmbedding):
         self.num_features = len(causality.vocab)
         self.num_hidden_features = len(causality.extended_vocab) - len(causality.vocab)
 
-        self.w = tf.keras.layers.Dense(self.config.attention_dim, use_bias=True, activation='tanh')
+        self.w = tf.keras.layers.Dense(
+            self.config.attention_dim, use_bias=True, activation="tanh"
+        )
         self.u = tf.keras.layers.Dense(1, use_bias=False)
 
         self._init_basic_embedding_variables(causality)
         self._init_embedding_mask(causality)
 
     def _init_basic_embedding_variables(self, causality: CausalityKnowledge):
-        logging.info('Initializing CAUSALITY basic embedding variables')
+        logging.info("Initializing CAUSALITY basic embedding variables")
         self.basic_feature_embeddings = self.add_weight(
             initializer=self._get_feature_initializer(
                 self._load_description_vocab(causality, set(causality.vocab.values()))
             ),
             trainable=self.config.base_feature_embeddings_trainable,
-            name='causality_embedding/basic_feature_embeddings',
-            shape=(self.num_features,self.config.embedding_dim),
+            name="causality_embedding/basic_feature_embeddings",
+            shape=(self.num_features, self.config.embedding_dim),
         )
         self.basic_hidden_embeddings = self.add_weight(
             initializer=self._get_hidden_initializer(
-                self._load_description_vocab(causality, set(causality.extra_vocab.values()))
+                self._load_description_vocab(
+                    causality, set(causality.extra_vocab.values())
+                )
             ),
             trainable=self.config.base_hidden_embeddings_trainable,
-            name='causality_embedding/basic_hidden_embeddings',
-            shape=(self.num_hidden_features,self.config.embedding_dim),
+            name="causality_embedding/basic_hidden_embeddings",
+            shape=(self.num_hidden_features, self.config.embedding_dim),
         )
 
-    def _load_description_vocab(self, causality: CausalityKnowledge, ids: Set[int]) -> Dict[int, str]:
-        return {idx:node.label_name for idx, node in causality.nodes.items() if idx in ids}
+    def _load_description_vocab(
+        self, causality: CausalityKnowledge, ids: Set[int]
+    ) -> Dict[int, str]:
+        return {
+            idx: node.label_name for idx, node in causality.nodes.items() if idx in ids
+        }
 
-    def _init_embedding_mask(self, causality: CausalityKnowledge): 
-        logging.info('Initializing CAUSALITY information')
+    def _init_embedding_mask(self, causality: CausalityKnowledge):
+        logging.info("Initializing CAUSALITY information")
         embedding_masks = {}
-        for idx, node in tqdm(causality.nodes.items(), desc='Initializing CAUSALITY information'):
-            if node.label_idx >= self.num_features: continue
+        for idx, node in tqdm(
+            causality.nodes.items(), desc="Initializing CAUSALITY information"
+        ):
+            if node.label_idx >= self.num_features:
+                continue
 
             neighbour_idxs = set(node.get_neighbour_label_idxs() + [idx])
             embedding_masks[idx] = [
@@ -57,51 +68,62 @@ class CausalityEmbedding(tf.keras.Model, BaseEmbedding):
             ]
 
         all_embedding_masks = [
-            tf.concat(embedding_masks[idx], axis=0)
-            for idx in range(self.num_features)
+            tf.concat(embedding_masks[idx], axis=0) for idx in range(self.num_features)
         ]
-        self.embedding_mask = tf.Variable(tf.expand_dims(
-            tf.concat([all_embedding_masks], axis=1),
-            axis=2
-        ), trainable=False)
+        self.embedding_mask = tf.Variable(
+            tf.expand_dims(tf.concat([all_embedding_masks], axis=1), axis=2),
+            trainable=False,
+        )
 
     def _load_full_embedding_matrix(self):
         return tf.repeat(
             tf.expand_dims(
                 tf.concat(
                     [self.basic_feature_embeddings, self.basic_hidden_embeddings],
-                    axis=0
-                ), # shape: (num_all_features, embedding_size)
-                axis=0
-            ), # shape: (1, num_all_features, embedding_size)
+                    axis=0,
+                ),  # shape: (num_all_features, embedding_size)
+                axis=0,
+            ),  # shape: (1, num_all_features, embedding_size)
             repeats=self.num_features,
-            axis=0
-        ) # shape: (num_features, num_all_features, embedding_size)
+            axis=0,
+        )  # shape: (num_features, num_all_features, embedding_size)
 
     def _load_attention_embedding_matrix(self):
         feature_embeddings = tf.repeat(
-            tf.expand_dims(self.basic_feature_embeddings, axis=1), # shape: (num_features, 1, embedding_size)
-            repeats=self.num_features+self.num_hidden_features,
+            tf.expand_dims(
+                self.basic_feature_embeddings, axis=1
+            ),  # shape: (num_features, 1, embedding_size)
+            repeats=self.num_features + self.num_hidden_features,
             axis=1,
-        ) # shape: (num_features, num_all_features, embedding_size)
+        )  # shape: (num_features, num_all_features, embedding_size)
         full_embeddings = self._load_full_embedding_matrix()
 
-        return tf.concat([feature_embeddings, full_embeddings], axis=2) # shape: (num_features, num_all_features, 2*embedding_size)
+        return tf.concat(
+            [feature_embeddings, full_embeddings], axis=2
+        )  # shape: (num_features, num_all_features, 2*embedding_size)
 
     def _calculate_attention_embeddings(self):
         full_embedding_matrix = self._load_full_embedding_matrix()
         attention_embedding_matrix = self._load_attention_embedding_matrix()
-        
+
         score = self.u(
             self.w(attention_embedding_matrix)
-        ) # shape: (num_features, num_all_features, 1)
+        )  # shape: (num_features, num_all_features, 1)
         score = tf.where(self.embedding_mask, tf.math.exp(score), 0)
-        score_sum = tf.reduce_sum(score, axis=1, keepdims=True) # shape: (num_features, 1, 1)
-        score_sum = tf.where(score_sum == 0, 1., score_sum)
+        score_sum = tf.reduce_sum(
+            score, axis=1, keepdims=True
+        )  # shape: (num_features, 1, 1)
+        score_sum = tf.where(score_sum == 0, 1.0, score_sum)
 
-        attention_weights = score / score_sum # shape: (num_features, num_all_features, 1)
-        context_vector = attention_weights * full_embedding_matrix  # shape: (num_features, num_all_features, embedding_size)
-        context_vector = tf.reduce_sum(context_vector, axis=1)  # shape: (num_features, embedding_size)
+        attention_weights = (
+            score / score_sum
+        )  # shape: (num_features, num_all_features, 1)
+        context_vector = (
+            attention_weights * full_embedding_matrix
+        )  # shape: (num_features, num_all_features, embedding_size)
+        context_vector = tf.reduce_sum(
+            context_vector, axis=1
+        )  # shape: (num_features, embedding_size)
 
         return (context_vector, attention_weights)
 
@@ -109,11 +131,17 @@ class CausalityEmbedding(tf.keras.Model, BaseEmbedding):
         context_vector, _ = self._calculate_attention_embeddings()
         return context_vector
 
-    def call(self, values): # values shape: (dataset_size, max_sequence_length, num_features)
+    def call(
+        self, values
+    ):  # values shape: (dataset_size, max_sequence_length, num_features)
         embedding_matrix = self._final_embedding_matrix()
-        return tf.linalg.matmul(values, embedding_matrix) # shape: (dataset_size, max_sequence_length, embedding_size)
+        return tf.linalg.matmul(
+            values, embedding_matrix
+        )  # shape: (dataset_size, max_sequence_length, embedding_size)
 
 
 class CausalityModel(BaseModel):
-    def _get_embedding_layer(self, metadata: SequenceMetadata, knowledge: CausalityKnowledge) -> tf.keras.Model:
+    def _get_embedding_layer(
+        self, metadata: SequenceMetadata, knowledge: CausalityKnowledge
+    ) -> tf.keras.Model:
         return CausalityEmbedding(knowledge, self.config)
