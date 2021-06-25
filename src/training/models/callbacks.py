baf61fe49bc0aa0ea67d9f1f1d03e1b4dd1e9988
lenaWitterauf <lena.emma77@gmail.com>
2021-06-25 09:37:44 +0100

    Use epoch as step when logging metrics to mlflow

--- src/training/models/callbacks.py
+++ src/training/models/callbacks.py
@@ -8,7 +8,7 @@ class MLFlowCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         mlflow.log_metric("epoch", epoch)
         for log_key in logs.keys():
-            mlflow.log_metric(log_key, logs[log_key])
+            mlflow.log_metric(key=log_key, value=logs[log_key], step=epoch)
 
 
 class BestModelRestoreCallback(tf.keras.callbacks.Callback):
