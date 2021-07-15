a51087ae5763d1efdfed33219137df3a36c53997
lenaWitterauf <lena.emma77@gmail.com>
2021-07-15 18:53:20 +0100

    Set default noise to none

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ class ExperimentConfig:
     n_epochs: int = 100
     sequence_type: str = 'mimic'
     model_type: str = 'simple'
-    noise_to_add: float = 0.1
+    noise_to_add: float = 0.0
     noise_to_remove: float = 0.0
     max_data_size: int = -1
     use_dataset_generator: bool = True
